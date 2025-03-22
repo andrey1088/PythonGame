bdf16f0fcd39b4c0835950c05d62b0bc80d51b7f
Andrey Musharov <a.musharov@dinarys.com>
2025-03-22 14:34:25 +0200

    update npc unique names

--- src/npc_generator/npc_generator.py
+++ src/npc_generator/npc_generator.py
@@ -4,53 +4,60 @@ import os
 import json
 
 # Localization setup
-locale_dir = os.path.join(os.path.dirname(__file__), 'locale')
+locale_dir = os.path.join(os.path.dirname(__file__), '../locale')
 gettext.bindtextdomain('messages', locale_dir)
 gettext.textdomain('messages')
 _ = gettext.gettext  # Translation function
 
-
 def get_data(key: str) -> list:
-    file = open(os.path.join(os.path.dirname(__file__), '../data/npc/npc.json'), 'r')
-    loaded_data = json.load(file)
-    result_list = []
+    try:
+        with open(os.path.join(os.path.dirname(__file__), '../data/npc/npc.json'), 'r', encoding='utf-8') as file:
+            loaded_data = json.load(file)
+            return loaded_data.get(key, [])
+    except FileNotFoundError:
+        print(_('Data file not found.'))
+        return []
+    except json.JSONDecodeError:
+        print(_('Error decoding JSON data.'))
+        return []
 
-    for item in loaded_data[key]:
-        result_list.append(item)
+available_male_names = get_data('male_names')
+available_female_names = get_data('female_names')
 
-    file.close()
-
-    return result_list
-
+def assign_unique_name(available_names):
+    name = random.choice(available_names)
+    if len(available_names) > 1:
+        available_names.remove(name)
+    return name
 
 class Person:
+    used_names = set()  # To track assigned names
+
     def __init__(self, npc_id):
         self.id = npc_id
-        self.role = get_data(key='roles')[npc_id]
-        self.gender = 'Femail' if self.role in get_data('femail_roles') else 'Mail'
+        self.role = get_data('roles')[npc_id]
+        self.gender = 'Female' if self.role in get_data('female_roles') else 'Male'
         self.personalities = []
-        self.pressure_response = [random.choice(get_data(key='pressure_response'))]
-
-        if self.gender == 'Femail':
-            self.name = random.choice(get_data(key='femail_names'))
+        self.pressure_response = [random.choice(get_data('pressure_response'))]
+        if self.gender == 'Female':
+            self.name = assign_unique_name(available_female_names)
             self.personalities = ['lover']
-            self.pressure_response.append('Flirt')
+            self.pressure_response.append('flirt')
         else:
-            self.name = random.choice(get_data(key='mail_names'))
-
-        self.race = random.choice(get_data(key='races')) if self.role in get_data(key='non_humans') else 'Human'
+            self.name = assign_unique_name(available_male_names)
+        self.race = random.choice(get_data('races')) if self.role in get_data('non_human_roles') else 'Human'
         self.personalities.append(random.choice(get_data(key='personalities')))
 
-        self.connections = {}  # Relationships between NPCs will be added later
         self.alibi = 'Unknown'
+        self.connections = {}
         self.knows_about = []
         self.suspect = False
 
     def __repr__(self):
-        return f'<Person {self.name} ({self.role}), {self.race}, {self.personalities}, {self.pressure_response}>'
+        return f'<Person {self.name} ({self.role}), {self.gender}, {self.race}, {self.personalities}, {self.pressure_response}>'
 
-# Generate 10 NPCs
-npc_list = [Person(i) for i in range(10)]
+# Generate NPCs
+npc_list = [Person(i) for i in range(len(get_data('roles')))]
 
 # Print NPC list
 for npc in npc_list:
